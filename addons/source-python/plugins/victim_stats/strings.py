ecd145343bd7d2779166ebf2ad38f94f426edae0
satoon101 <satoon101@gmail.com>
2018-08-05 18:45:23 -0400

    Moved strings into victim_stats.strings module.
    Removed engine-specific translations.
    
    Reorganized strings into strings/config_strings translation files.

---
+++ addons/source-python/plugins/victim_stats/strings.py
@@ -0,0 +1,28 @@
+# ../victim_stats/strings.py
+
+"""Contains all translation variables for the base plugin."""
+
+# =============================================================================
+# >> IMPORTS
+# =============================================================================
+# Source.Python
+from translations.strings import LangStrings
+
+# Plugin
+from .info import info
+
+
+# =============================================================================
+# >> ALL DECLARATION
+# =============================================================================
+__all__ = (
+    'CONFIG_STRINGS',
+    'TRANSLATION_STRINGS',
+)
+
+
+# =============================================================================
+# >> GLOBAL VARIABLES
+# =============================================================================
+CONFIG_STRINGS = LangStrings(f'{info.name}/config_strings')
+TRANSLATION_STRINGS = LangStrings(f'{info.name}/strings')
